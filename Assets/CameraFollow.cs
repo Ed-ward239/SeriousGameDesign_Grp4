150fdcb86670437426e86c7ce2f3738ceeb4c597
pooria <pooria@drsnwk.com>
2022-11-06 23:01:06 -0500

    some assets and camera follow script added

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    // public Vector3 offset;
+    [Range(1, 10)]
+    public float smoothFactor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    
+    void FixedUpdate() {
+        // Vector3 targetPosition = new Vector3(target.position.x, target.position.y, -10.0f);
+        // for horizontal follow only add this instead.
+         Vector3 targetPosition = new Vector3(target.position.x, transform.position.y, -10.0f);
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.fixedDeltaTime);
+        transform.position = smoothPosition;
+    }
+
+}
