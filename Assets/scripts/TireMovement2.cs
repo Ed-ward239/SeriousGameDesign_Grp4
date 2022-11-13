bf47149bb840d366baf3298801026ab517860ddc
pooria <pooria@drsnwk.com>
2022-11-13 14:37:57 -0500

    main road added

--- Assets/scripts/TireMovement2.cs
+++ Assets/scripts/TireMovement2.cs
@@ -9,6 +9,7 @@ public class TireMovement2 : MonoBehaviour
     public Rigidbody2D backTire;
     [SerializeField] int speed = 100;
     [SerializeField] float movement;
+    [SerializeField] bool brake = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,16 +20,29 @@ public class TireMovement2 : MonoBehaviour
     void Update()
     {
         movement = Input.GetAxis("Horizontal");
+
+        if(Input.GetButtonDown("Jump")) {
+            brake = true;
+        }
+
+        if(!Input.GetButtonDown("Jump")) {
+            brake = false;
+        }
     }
 
     void FixedUpdate() {
         frontTire.AddTorque(speed * -movement * Time.fixedDeltaTime);
         backTire.AddTorque(speed * -movement * Time.fixedDeltaTime);
 
-        while(Input.GetButtonDown("Jump")) {
+        if(Input.GetButtonDown("Jump")) {
             frontTire.AddTorque(0);
         backTire.AddTorque(0);
 
         }
+    }
+
+    void Brake() {
+           frontTire.AddTorque(0);
+        backTire.AddTorque(0);
     }
 }
