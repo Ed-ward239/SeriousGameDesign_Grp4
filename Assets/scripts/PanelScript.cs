c6dd135504a951cc6159edf3bad5ad764753fdf9
Mohammed Fadel <fadel@Mohammeds-MacBook-Pro.local>
2022-11-15 18:00:05 -0500

    updating

---
+++ Assets/scripts/PanelScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelScript : MonoBehaviour
+{
+    public GameObject Panel;
+    public GameObject Car;
+    public GameObject Sign;
+
+    
+    public void vanishCar(){
+        if(Car!= null){
+            Car.SetActive(false);
+        }
+    }
+    public void appearPanel(){ //turns panel off/on
+        if(Panel!= null){
+           // bool isActive = Panel.activeSelf;
+            Panel.SetActive(true);
+            vanishCar();
+        }
+    }    
+    private void OnTriggerEnter2D(Collider2D collider){
+        Sign.GetComponent<BoxCollider2D> ().enabled = false;
+        Debug.Log("Trigger Panel!");
+        appearPanel();
+        StartCoroutine(EnableBox(20.0F));
+    }
+ 
+    IEnumerator EnableBox(float waitTime) {
+         yield return new WaitForSeconds(waitTime);
+         Sign.GetComponent<BoxCollider2D> ().enabled = true;
+    }
+    
+    
+}
