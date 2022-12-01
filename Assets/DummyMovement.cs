71d2f1f47502db145e847199ae0aefaaf0679131
Raekwon Williams <Raekwill15@gmail.com>
2022-12-01 16:53:56 -0500

    added Fadeout to DummyMovement

--- Assets/DummyMovement.cs
+++ Assets/DummyMovement.cs
@@ -8,9 +8,16 @@ public class DummyMovement : MonoBehaviour
     [SerializeField] Animator animator;
     [SerializeField] AudioSource audio;
     [SerializeField] bool hasContacted = false;
+    
+    private bool fadeIn,fadeOut;
+    public float fadeSpeed;
+
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("body");
+
         if (dummy == null) {
             dummy = GetComponent<Rigidbody2D>();
         }
@@ -27,6 +34,22 @@ public class DummyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (dummy.position.x - player.GetComponent<Rigidbody2D>().position.x < 15) {
+            FadeOutObject();
+        }
+
+        if (fadeOut) {
+            Color objectColor = this.GetComponent<Renderer>().material.color;
+            float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g,objectColor.b, fadeAmount);
+            this.GetComponent<Renderer>().material.color = objectColor;
+
+            if (objectColor.a <= .1) {
+                fadeOut = false;
+                Kill();
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collider) {
@@ -49,4 +72,13 @@ public class DummyMovement : MonoBehaviour
     void Kill() {
         Destroy(animator.gameObject);
     }
+
+    public void FadeOutObject() {
+        fadeOut = true;
+    }
+
+    public void FadeInObject() {
+        fadeIn = true;
+    }
+
 }
