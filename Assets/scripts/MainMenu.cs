46c52cd46668dba82ea3f11eb904769aeedc11ee
Mohammed Fadel <fadel@Mohammeds-MacBook-Pro.local>
2022-12-07 11:13:28 -0500

    uploading

--- Assets/scripts/MainMenu.cs
+++ Assets/scripts/MainMenu.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class MainMenu : MonoBehaviour
 {
+
+    [SerializeField] TMP_InputField playerNameInput;
+
     public void Play()
     {
+        string name = playerNameInput.text;
+        PersistentData.Instance.SetName(name);
+        PersistentData.Instance.SetScore(0);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void Quit()
